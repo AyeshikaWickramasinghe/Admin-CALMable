3b1c5e1ba1a70759a874ce4c8b0dc29c2fe60c13
AyeshikaWickramasinghe <35-se-0035@kdu.ac.lk>
2021-11-30 11:05:53 +0530

    Admin add music

---
+++ app/src/main/java/com/example/admincalmable/Model/UploadImage.java
@@ -0,0 +1,20 @@
+package com.example.admincalmable.Model;
+
+public class UploadImage {
+    private String imageUrl;
+
+    public UploadImage() {
+    }
+
+    public UploadImage(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
